89bf152ea06a6f6efa482dbcfdddea2a360c510d
José Carlos Valero Sánchez <josecarlos@boclips.com>
2019-05-09 11:03:12 +0100

    Add Spring Boot Starter
    [#165883440]

---
+++ src/main/kotlin/com/boclips/apidocs/ApiDocsApplication.kt
@@ -0,0 +1,11 @@
+package com.boclips.apidocs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiDocsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiDocsApplication>(*args)
+}
