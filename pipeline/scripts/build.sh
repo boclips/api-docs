5b67ef79f35f7c0b400fd97358bd2fe03e82a521
Antony O'Neill <antony@boclips.com>
2019-06-03 09:52:58 +0100

    wip

--- pipeline/scripts/build.sh
+++ pipeline/scripts/build.sh
@@ -7,7 +7,7 @@ export GRADLE_USER_HOME="$(pwd)/.gradle"
 version=$(cat version/version)
 
 (
-cp player-source/docs/player-guide.adoc source/src/docs/asciidoc/
+player-source/scripts/prepareDoc.sh source/src/docs/asciidoc/
 
 cd source
 ./gradlew -Pversion=${version} clean build --rerun-tasks --no-daemon
