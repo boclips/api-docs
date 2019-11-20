f6ec6a4cbe21b8c6be405ad5b3c1457dd1e6f8df
Sebastian Kazenbroot-Guppy <sebastian@boclips.com>
2019-11-20 16:41:45 +0000

    Documented user profile getting and updating.
    [#169431774]
    
    Signed-off-by: Rafal Lewandowski <rafal@boclips.com>

--- src/test/kotlin/com/boclips/apidocs/UserDocTests.kt
+++ src/test/kotlin/com/boclips/apidocs/UserDocTests.kt
@@ -1,12 +1,16 @@
 package com.boclips.apidocs
 
 import com.boclips.apidocs.testsupport.AbstractDocTests
+import com.boclips.apidocs.testsupport.RequestSpecificationFactory
 import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
+import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel
 import org.springframework.restdocs.hypermedia.HypermediaDocumentation.links
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath
 import org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document
@@ -23,7 +27,7 @@ class UserDocTests : AbstractDocTests() {
                         fieldWithPath("firstName").description("The first name of the user"),
                         fieldWithPath("lastName").description("The last name of the user"),
                         fieldWithPath("ages").description("A list of ages that the user teaches"),
-                        fieldWithPath("subjects").description("A list subjects that the user teaches"),
+                        subsectionWithPath("subjects").description("A list subjects that the user teaches"),
                         fieldWithPath("email").description("The email of the user"),
                         fieldWithPath("analyticsId").ignored(),
                         fieldWithPath("organisationAccountId").ignored(),
@@ -38,6 +42,46 @@ class UserDocTests : AbstractDocTests() {
             )
             .`when`()
             .get(links["profile"])
+            .then()
+            .assertThat().statusCode(`is`(200))
+    }
+
+    @Test
+    fun `updating user profile`(restDocumentation: RestDocumentationContextProvider) {
+        val spec = RequestSpecificationFactory.createFor(updatableClientAccessToken, restDocumentation)
+        val subject = videoServiceClient.subjects[0].id.value
+        val myLinks = getLinksFor(updatableClientAccessToken)
+
+        given(spec)
+            .filter(
+                document(
+                    "resource-user-put-user-profile",
+                    requestFields(
+                        fieldWithPath("firstName").optional().description("The user's first name"),
+                        fieldWithPath("lastName").optional().description("The user's last name"),
+                        fieldWithPath("subjects").optional().description("The subjects of the user, by name"),
+                        fieldWithPath("ages").optional().description("The ages taught by the user"),
+                        fieldWithPath("country").optional().description("The country of the user (3-letter ISO Country Code)"),
+                        fieldWithPath("state").optional().description("The state of the user (2-letter, US only)")
+
+                    )
+                )
+            )
+            .`when`()
+            .contentType(ContentType.JSON)
+            .body(
+                """
+                {
+                    "firstName": "Some First Name",
+                    "lastName": "Some Last Name",
+                    "subjects": ["$subject"],
+                    "ages": [7,8,9],
+                    "country": "USA",
+                    "state": "AZ"
+                }
+            """.trimIndent()
+            )
+            .put(myLinks["profile"])
             .then()
             .assertThat().statusCode(`is`(200))
     }
