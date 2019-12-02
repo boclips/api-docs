43def172dfac52c6f417e63f94ec090812813cc1
Remigiusz Oleszczuk <remy@boclips.com>
2019-12-02 11:54:07 +0100

    Add a section on attachments and link to it from collection fields table
    [#166032736]
    
    Signed-off-by: Rafal Lewandowski <rafal@boclips.com>

--- src/test/kotlin/com/boclips/apidocs/CollectionsDocTests.kt
+++ src/test/kotlin/com/boclips/apidocs/CollectionsDocTests.kt
@@ -330,7 +330,7 @@ class CollectionsDocTests : AbstractDocTests() {
                         fieldWithPath("createdBy").description("Name of collection's creator"),
                         fieldWithPath("subjects").description("A list of teaching subjects this collection relates to"),
                         fieldWithPath("ageRange").description("Tells which ages videos in this collection are suitable for"),
-                        fieldWithPath("attachments").description("A list of items attached to this collection"),
+                        fieldWithPath("attachments").description("A list of <<resources-collections-attachments,attachments>> linked to this collection"),
                         subsectionWithPath("_links").description("HAL links related to this collection")
                     ),
                     links(
