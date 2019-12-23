d3bfef97c7aa583f3421f1908a27f80b9501e18f
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2019-12-23 14:38:21 +0100

    Rearrange and group field documentation in a more meaningful way

--- src/test/kotlin/com/boclips/apidocs/GatewayDocTests.kt
+++ src/test/kotlin/com/boclips/apidocs/GatewayDocTests.kt
@@ -8,29 +8,29 @@ import org.junit.jupiter.api.Test
 import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel
 import org.springframework.restdocs.hypermedia.HypermediaDocumentation.links
-import org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document
 
-class GatewayDocTests: AbstractDocTests() {
+class GatewayDocTests : AbstractDocTests() {
 
     @Test
     fun `error example`() {
         given(documentationSpec)
-                .filter(
-                        document(
-                                "error-example"
-                                , responseFields(
-                                fieldWithPath("error").description("The HTTP error that occurred, e.g. `Invalid field`"),
-                                fieldWithPath("message").description("A description of the cause of the error"),
-                                fieldWithPath("path").description("The path to which the request was made"),
-                                fieldWithPath("status").description("The HTTP status code, e.g. `400`"),
-                                fieldWithPath("timestamp").description("The time at which the error occurred"))
-                        )
+            .filter(
+                document(
+                    "error-example"
+                    , responseFields(
+                        fieldWithPath("error").description("The HTTP error that occurred, e.g. `Invalid field`"),
+                        fieldWithPath("message").description("A description of the cause of the error"),
+                        fieldWithPath("path").description("The path to which the request was made"),
+                        fieldWithPath("status").description("The HTTP status code, e.g. `400`"),
+                        fieldWithPath("timestamp").description("The time at which the error occurred")
+                    )
                 )
-                .`when`().get("/videos?duration_min=not-quite-a-number").apply { prettyPrint() }
-                .then().assertThat().statusCode(`is`(400))
-
+            )
+            .`when`().get("/videos?duration_min=not-quite-a-number").apply { prettyPrint() }
+            .then().assertThat().statusCode(`is`(400))
     }
 
     @Test
@@ -41,37 +41,37 @@ class GatewayDocTests: AbstractDocTests() {
             .filter(
                 document(
                     "resource-index"
-                , links(
-                linkWithRel("activate").description("A `PUT` request against this link allows to pass profile information and activate a new user"),
-                linkWithRel("trackPageRendered").description("..."),
+                    , links(
+                        linkWithRel("trackPageRendered").description("`POST` endpoint for tracking pageRendered event"),
+                        linkWithRel("createPlaybackEvents").description("Sending <<_sending_a_batch_of_playback_events,batches>> of playback events from the past"),
 
-                linkWithRel("video").description("The video resource, templated link to retrieve an individual video"),
-                linkWithRel("searchVideos").description("Templated link to perform video search"),
+                        linkWithRel("activate").description("A `PUT` request against this link allows to pass profile information and activate a new user"),
+                        linkWithRel("profile").description("Templated link to get user profile information"),
 
-                linkWithRel("trackPageRendered").description("`POST` endpoint for tracking pageRendered event"),
+                        linkWithRel("video").description("The video resource, templated link to retrieve an individual video"),
+                        linkWithRel("searchVideos").description("Templated link to perform video search"),
 
-                linkWithRel("collection").description("The collection resource, templated link to retrieve an individual video collection"),
-                linkWithRel("createCollection").description("Link to create a new video collection"),
-                linkWithRel("myCollections").description("Collections created by the current user"),
-                linkWithRel("publicCollections").description("Collections marked as public"),
-                linkWithRel("searchPublicCollections").description("Search all public collections"),
-                linkWithRel("searchCollections").description("Search all collections"),
-                linkWithRel("bookmarkedCollections").description("Collections bookmarked by the current user"),
+                        linkWithRel("videoTypes").description("Lists <<resources-video-types,types>> of videos available in the system. These can be later used when <<resources-video-search,searching>>"),
 
-                linkWithRel("contentPartner").description("Retrieve a specific content partner"),
-                linkWithRel("contentPartners").description("Retrieve all content partners"),
+                        linkWithRel("tags").description("List of tags that can be attached to videos"),
 
-                linkWithRel("subjects").description("List of subjects available"),
-                linkWithRel("profile").description("Templated link to get user profile information"),
-                linkWithRel("countries").description("List of countries"),
+                        linkWithRel("collection").description("The collection resource, templated link to retrieve an individual video collection"),
+                        linkWithRel("createCollection").description("Link to create a new video collection"),
+                        linkWithRel("myCollections").description("Collections created by the current user"),
+                        linkWithRel("publicCollections").description("Collections marked as public"),
+                        linkWithRel("searchPublicCollections").description("Search all public collections"),
+                        linkWithRel("searchCollections").description("Search all collections"),
+                        linkWithRel("bookmarkedCollections").description("Collections bookmarked by the current user"),
 
-                linkWithRel("tags").description("List of tags that can be attached to videos"),
-                linkWithRel("videoTypes").description("Lists <<resources-video-types,types>> of videos available in the system. These can be later used when <<resources-video-search,searching>>"),
+                        linkWithRel("subjects").description("List of subjects available"),
 
-                linkWithRel("disciplines").description("List of disciplines available in the system (e.g. arts, humanities...)"),
+                        linkWithRel("disciplines").description("List of disciplines available in the system (e.g. arts, humanities...)"),
 
-                linkWithRel("createPlaybackEvents").description("Sending <<_sending_a_batch_of_playback_events,batches>> of playback events from the past")
-            )
+                        linkWithRel("countries").description("List of countries"),
+
+                        linkWithRel("contentPartner").description("Retrieve a specific content partner"),
+                        linkWithRel("contentPartners").description("Retrieve all content partners")
+                    )
                 )
             )
             .`when`().get("/").apply { prettyPrint() }
