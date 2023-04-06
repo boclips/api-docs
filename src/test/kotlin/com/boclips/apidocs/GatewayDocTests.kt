7cec658d48331d90371621fbc46786dd9615faed
Eva Varga <eva@boclips.com>
2023-04-06 14:26:01 +0200

    Remove info about non existing links
    [#184526975]
    
    Signed-off-by: Maciej Idzikowski <maciej@boclips.com>

--- src/test/kotlin/com/boclips/apidocs/GatewayDocTests.kt
+++ src/test/kotlin/com/boclips/apidocs/GatewayDocTests.kt
@@ -90,10 +90,8 @@ class GatewayDocTests : AbstractDocTests() {
                         linkWithRel("suggestions").ignored(),
                         linkWithRel("getMetadata").ignored(),
                         linkWithRel("getAllProviders").description("List all providers and their types"),
-                        linkWithRel("getAllTypesByProvider").ignored(),
                         linkWithRel("getThemesByProviderAndId").ignored(),
                         linkWithRel("getThemesByIds").ignored(),
-                        linkWithRel("getThemeById").ignored(),
                         linkWithRel("getThemesByProvider").description("List all available theme for a specific provider"),
                         linkWithRel("access").ignored(),
                         linkWithRel("license").ignored()
