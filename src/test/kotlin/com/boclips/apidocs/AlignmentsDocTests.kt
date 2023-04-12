5e03e8e826ab02631b0f7744cb786211dafa9ca3
Arfaz Feroz <arfaz@boclips.com>
2023-04-12 11:46:50 +0100

    Create test to get theme by provider and id

--- src/test/kotlin/com/boclips/apidocs/AlignmentsDocTests.kt
+++ src/test/kotlin/com/boclips/apidocs/AlignmentsDocTests.kt
@@ -1,9 +1,11 @@
 package com.boclips.apidocs
 
 import com.boclips.apidocs.testsupport.AbstractDocTests
+import com.damnhandy.uri.template.UriTemplate
 import io.restassured.RestAssured
 import org.hamcrest.CoreMatchers
 import org.junit.jupiter.api.Test
+import org.springframework.restdocs.payload.PayloadDocumentation
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import org.springframework.restdocs.request.RequestDocumentation
@@ -37,11 +39,10 @@ class AlignmentsDocTests : AbstractDocTests() {
     fun `get a provider themes`() {
         RestAssured.given(stubOwnerSpec)
             .filter(
-
                 RestAssuredRestDocumentation.document(
                     "resource-provider-themes-get",
                     RequestDocumentation.pathParameters(
-                        RequestDocumentation.parameterWithName("provider").description("Id of the provider")
+                        RequestDocumentation.parameterWithName("provider").description("ID of the provider")
                     ),
                     responseFields(
                         fieldWithPath("_embedded.themes[].id").description("ID of the theme"),
@@ -62,6 +63,41 @@ class AlignmentsDocTests : AbstractDocTests() {
             )
             .`when`()
             .get(links["getThemesByProvider"], "ngss").apply { prettyPrint() }
+            .then()
+            .assertThat().statusCode(CoreMatchers.`is`(200))
+    }
+
+    @Test
+    fun `get a provider theme by id`() {
+        val theme = alignmentClient.getThemes()._embedded.themes[0]
+        RestAssured.given(stubOwnerSpec)
+            .filter(
+                RestAssuredRestDocumentation.document(
+                    "resource-provider-theme-get",
+                    RequestDocumentation.pathParameters(
+                        RequestDocumentation.parameterWithName("provider").description("ID of the provider"),
+                        RequestDocumentation.parameterWithName("id").description("ID of the theme")
+                    ),
+                    responseFields(
+                        fieldWithPath("id").description("ID of the theme"),
+                        fieldWithPath("provider")
+                            .description("Provider name"),
+                        fieldWithPath("_links")
+                            .ignored(),
+                        fieldWithPath("type").description("Name of the type"),
+                        fieldWithPath("title").description("Name of the theme"),
+                        fieldWithPath("topics[].index").description("Recommended order of the topic"),
+                        fieldWithPath("topics[].title").description("Topic name"),
+                        fieldWithPath("topics[].targets[].index").description("Recommended order of the target"),
+                        fieldWithPath("topics[].targets[].title").description("Target name"),
+                        fieldWithPath("topics[].targets[].videoIds").description("Videos aligned to the target"),
+                        PayloadDocumentation.subsectionWithPath("topics[].targets[].videos").description("Videos details"),
+                        fieldWithPath("logoUrl").ignored()
+                    )
+                )
+            )
+            .`when`()
+            .get(links["getThemesByProviderAndId"], theme.provider, theme.id).apply { prettyPrint() }
             .then()
             .assertThat().statusCode(CoreMatchers.`is`(200))
     }
