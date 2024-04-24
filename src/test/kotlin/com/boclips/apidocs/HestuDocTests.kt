2f5fcf15f3e7869d330a366cd31433b0ec64fdc8
Alex Glen <alex@boclips.com>
2024-04-24 16:56:37 +0100

    Add versioning headers and descriptions to hestu docs
    [sc-1253]

--- src/test/kotlin/com/boclips/apidocs/HestuDocTests.kt
+++ src/test/kotlin/com/boclips/apidocs/HestuDocTests.kt
@@ -7,22 +7,32 @@ import io.restassured.RestAssured
 import io.restassured.specification.RequestSpecification
 import org.hamcrest.CoreMatchers
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.headers.HeaderDocumentation
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath
 import org.springframework.restdocs.request.RequestDocumentation
 import org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document
 import org.springframework.restdocs.snippet.Attributes
 
 class HestuDocTests : AbstractDocTests() {
     private val productionVideoId = "5c54d67ed8eafeecae2041c2"
+    private val productionHighlightId = "CLPOTj69sapco"
     private val LEARNING_OUTCOMES_PATH = "/v1/videos/{video_id}/learning-outcomes"
     private val ASSESSMENT_QUESTIONS_PATH = "/v1/videos/{video_id}/assessment-questions"
     private val HIGHLIGHT_PATH = "/v1/highlights/{highlightId}"
     private val HIGHLIGHTS_PATH = "/v1/highlights"
     lateinit var hestuDocumentationSpec: RequestSpecification
+
+    val versionHeaders = HeaderDocumentation.requestHeaders(
+        HeaderDocumentation
+            .headerWithName("X-Boclips-Api-Version")
+            .optional()
+            .description("Specify a version of this Endpoint in YYYY-MM-DD format. We strongly advise sending this header to protect against breaking changes")
+
+    )
 
     @BeforeEach
     fun setupDocumentationSpec(restDocumentation: RestDocumentationContextProvider) {
@@ -99,10 +109,18 @@ class HestuDocTests : AbstractDocTests() {
     }
 
     @Test
-    @Disabled
     fun `Get highlight`(restDocumentation: RestDocumentationContextProvider) {
         val responseFields = responseFields(
-            fieldWithPath("getHighlight").description("Get a given highlight by ID"),
+            fieldWithPath("id").description("ID of the highlight"),
+            fieldWithPath("video_id").description("ID of the highlight's video"),
+            fieldWithPath("title").description("Title of the highlight"),
+            fieldWithPath("caption").description("Captions in the highlight"),
+            fieldWithPath("start_time").description("Start time of the highlight"),
+            fieldWithPath("end_time").description("End  time of the highlight"),
+            fieldWithPath("duration").description("Duration of the highlight"),
+            fieldWithPath("score").description("Confidence score of the highlight"),
+            subsectionWithPath("_links").description("HAL links for the highlight")
+
         )
 
         val pathParameters = RequestDocumentation.pathParameters(
@@ -113,6 +131,7 @@ class HestuDocTests : AbstractDocTests() {
             .filter(
                 document(
                     "highlight-get",
+                    versionHeaders,
                     pathParameters,
                     responseFields,
                 ),
@@ -126,50 +145,55 @@ class HestuDocTests : AbstractDocTests() {
                 ),
             )
             .`when`()
-            .get(HIGHLIGHT_PATH, productionVideoId).apply { prettyPrint() }
+            .get(HIGHLIGHT_PATH, productionHighlightId).apply { prettyPrint() }
             .then()
             .assertThat().statusCode(CoreMatchers.`is`(200))
     }
 
     @Test
-    @Disabled
     fun `Get highlights`(restDocumentation: RestDocumentationContextProvider) {
         val responseFields = responseFields(
-            fieldWithPath("getHighlights").description("Get filtered highlights"),
+            fieldWithPath("_embedded.highlights[].id").description("ID of the highlight"),
+            fieldWithPath("_embedded.highlights[].video_id").description("ID of the highlight's video"),
+            fieldWithPath("_embedded.highlights[].title").description("Title of the highlight"),
+            fieldWithPath("_embedded.highlights[].caption").description("Captions in the highlight"),
+            fieldWithPath("_embedded.highlights[].start_time").description("Start time of the highlight"),
+            fieldWithPath("_embedded.highlights[].end_time").description("End  time of the highlight"),
+            fieldWithPath("_embedded.highlights[].duration").description("Duration of the highlight"),
+            fieldWithPath("_embedded.highlights[].score").description("Confidence score of the highlight"),
+            subsectionWithPath("_embedded.highlights[]._links").description("HAL links for the highlight"),
         )
-
-        // TODO("Review the types, descriptions, and optionality of these params w/ DS)
         val requestParameters = RequestDocumentation.requestParameters(
+            RequestDocumentation.parameterWithName("query")
+                .description("The text search query")
+                .attributes(Attributes.key("type").value("String")),
             RequestDocumentation.parameterWithName("threshold")
                 .optional()
-                .description("")
+                .description("Minimum similarity score a clip needs to be returned between 0 and 1. Default is none, which will return the closest match no matter what.")
                 .attributes(Attributes.key("type").value("Number")),
             RequestDocumentation.parameterWithName("size")
                 .optional()
-                .description("")
+                .description("The number of highlights to return, 100 by default")
                 .attributes(Attributes.key("type").value("Number")),
-
-            RequestDocumentation.parameterWithName("query")
-                .optional()
-                .description("")
-                .attributes(Attributes.key("type").value("String")),
 
             RequestDocumentation.parameterWithName("education_level")
                 .optional()
-                .description("")
+                .description("Filter by education level. Multiple values can be specified (comma separated, or by repeating the parameter). See possible values at <<resources-education-levels,education levels resource>>")
                 .attributes(Attributes.key("type").value("String")),
 
-            RequestDocumentation.parameterWithName("max_allowed_clip_overlap")
+            RequestDocumentation.parameterWithName("max_overlap")
                 .optional()
-                .description("")
+                .description("If multiple highlights come from the same video, this is the maximum they are allowed to overlap as a % (expressed as a decimal e.g. 0.2 is 20%) of the smallest video. If 1 a highlight can be a full segment of another highlight. If 0 no overlap is allowed.")
                 .attributes(Attributes.key("type").value("String"))
         )
         RestAssured.given(hestuDocumentationSpec)
             .filter(
                 document(
                     "highlights-get",
+                    versionHeaders,
                     requestParameters,
                     responseFields,
+
                 ),
             )
             .filter(
@@ -181,7 +205,7 @@ class HestuDocTests : AbstractDocTests() {
                 ),
             )
             .`when`()
-            .get(HIGHLIGHTS_PATH).apply { prettyPrint() }
+            .get("$HIGHLIGHTS_PATH?query=test").apply { prettyPrint() }
             .then()
             .assertThat().statusCode(CoreMatchers.`is`(200))
     }
